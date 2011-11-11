95af58c9406a2df05bd811935b2703e8318c38aa
blindgaenger <blindgaenger@gmail.com>
2011-11-11 13:47:48 +0100

    load the config from a file

--- lib/brot.rb
+++ lib/brot.rb
@@ -2,16 +2,22 @@ require 'brot/version'
 require 'brot/bot'
 require 'protolink'
 require 'slop'
+require 'yaml'
 
 module Brot
   extend self
 
-  def start(options)
-    protonet = Protolink::Protonet.open(options[:server], options[:username], options[:password])
+  def start(config)
+    protonet = Protolink::Protonet.open(config['server'], config['username'], config['password'], config['proxy'])
     puts "connected to protonet"
 
-    bot = Bot.new(protonet)
-    bot.listen
+    bot_configs = config['bots']
+    puts "found #{bot_configs.size} bots"
+
+    bot_configs.each do |bot_config|
+      bot = Bot.new(protonet, bot_config)
+      bot.listen # TODO: don't stop here
+    end
   end
 
 end
--- lib/brot/bot.rb
+++ lib/brot/bot.rb
@@ -1,15 +1,26 @@
 module Brot
-  class Bot
 
-    def initialize(protonet)
+  class Bot
+    TRIGGER_MESSAGE = 'meep.receive'
+
+    def initialize(protonet, config)
       @protonet = protonet
+      @name = config['name']
+      @user = @protonet.current_user
+      @channel = @protonet.find_channel(config['channel_id'])
     end
 
     def listen
-      puts 'brot starting to listen ...'
+      puts "#{@name} starts to listen on channel #{@channel.name} ..."
       @protonet.socket do |json|
-        next unless json['trigger'] == 'meep.receive'
-        puts json.inspect
+        next if json['user_id'] == @user.id
+        next unless json['channel_id'] == @channel.id
+        next unless json['trigger'] == TRIGGER_MESSAGE
+
+        author = json['author']
+        message = json['message']
+
+        @channel.speak("#{author} said: #{message}")
       end
     end
 
